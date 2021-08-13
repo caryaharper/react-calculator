9f65924c73e2e77689084412fea383a31bcc1f38
caryaharper <caryaharper@gmail.com>
2021-08-13 17:03:03 -0500

    Refactored button function to add css classes

--- src/button.js
+++ src/button.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 
 const Button = ({ value, handler }) => {
-    return <button onClick={() => handler(value)}>value</button>
+    let buttonType = 'op-buttons';
+
+    if (Number.isInteger(value) || value === '.') {
+        buttonType = 'int-buttons'
+    } 
+    
+    if(value === 'C' || value === '±' || value === '÷') {
+        buttonType = 'spec-buttons';
+    }
+
+    return <button className={`buttons ${buttonType}`} onClick={() => handler(value)}>value</button>
 }
 
 export default Button;
